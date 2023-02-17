87cf049429b071728eedb261f8090de95bee492a
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2023-02-16 20:25:32 -0800

    adding read me and fixing the save btn

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
-# mydailyplanner
-
-
-## User Story
-
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time.
-
-This daily planer is being mad to set a scheduled list of Items for the work day.
-
-This is being run with HTML and JavaScript to make the functions run smooth and efficient,
-it is being styled with the help of the CSS witch include the colors, button sizes and the text on the page.
-
-This includes styles produce in the HTML as well. 
-
-This planer will include the day, the date and the current time when you open the planer, as the day goes by the colors of the hour container shold change from current=red to grey=past and future=green.
-
+# My Daily Planner
+![licenseBadge](https://img.shields.io/badge/License-MIT-green)
+## Description 
+This app is to plan and track your daily work.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Questions](#questions)
+## Installation 
+To install necessary dependencies, run the following command: 
+```
+You need to open by right clicking on the vs code page and opening in default browser.
+```
+## Usage 
+You can track your daily work and make notes through out the day.
+## License
+      
+This project is licensed under the MIT license.
+## Contributors
+N/A
+## Test
+To run tests, run the following command: 
+```
+N/A
+```
+## Questions
+If you have any questions about this project, contact me directly at Jonathanjair@yahoo.com. You can find more of my work at [FregosoJ](https://github.com/FregosoJ/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//MY PSEUDOCODE
+
+// this example is best to do static element vs dynamic
+// time/date
+// save user entry to local storage
+// if current time, block is red, if future time, green, if past time, gray (similar to timer from quiz, checking every second)
+// go to html and finish off other timeblocks √
+// as this runs, loop through timeblocks, check (id, id should be military, set id as 12, 1, 3, set a class)
+// queryselectorall(class of timeblock), if id is less, its past, if its === its current, if its > its future
+// add event listener to save button √
+// put date/time on page √
+// start interval √
+// checking time for adjusting timeblocks color
+// check local storage to see if we need to save anything on timeblocks
+// attach eventListeners to all buttons
+// best practice - keep variables where you need them as opposed to all being globally scoped
+
+// jQuery
 var timeBlockEl = $(".time-block");
 var today = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
 var currentHour = dayjs().hour();
@@ -12,6 +30,7 @@ for (var i = 0; i < timeBlockEl.length; i++) {
 
 
 // FUNCTION TO UPDATE COLOR BLOCK--SHOULD THIS BE RUNNING WITH TIMER/CLOCK? -- HOW DO I KNOW IF THIS IS WORKING?
+
 function checkTime() {
   for (var i = 0; i < hourTimeNumber.length; i++)
     if (hourTimeNumber[i] < currentHour) {
@@ -24,6 +43,7 @@ function checkTime() {
 }
 
 // FUNCTION TO GET FROM LOCAL STORAGE AND DISPLAY USER INPUT ON TEXTAREA
+
 function renderUserInput() {
 
   for (var i = 0; i < timeBlockEl.length; i++) {
@@ -34,6 +54,7 @@ function renderUserInput() {
 }
 
 // FUNCTION TO SAVE USER INPUT ON TEXTAREA
+
 function saveUserInput() {
   var value = $(this).siblings(".description").val();
   var key = $(this).parent().attr("id");
@@ -41,6 +62,8 @@ function saveUserInput() {
 }
 
 // TIME INTERVAL FUNCTION
+
+
 setInterval(function () {
   var today = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
   $("#currentDay").text(today);
@@ -50,22 +73,3 @@ $(".saveBtn").on("click", saveUserInput);
 
 checkTime();
 renderUserInput();
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
